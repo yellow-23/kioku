173846043b84672731458c83dde044cea76055a8
yellow-23 <c.floresvillegas1@uandresbello.edu>
2026-09-08 09:56:11 -0300

    Initial Commit

---
+++ kiokuTests/kiokuTests.swift
@@ -0,0 +1,18 @@
+//
+//  kiokuTests.swift
+//  kiokuTests
+//
+//  Created by Cristobal Flores Villegas on 08-09-26.
+//
+
+import Testing
+
+struct kiokuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ kiokuUITests/kiokuUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  kiokuUITestsLaunchTests.swift
+//  kiokuUITests
+//
+//  Created by Cristobal Flores Villegas on 08-09-26.
+//
+
+import XCTest
+
+final class kiokuUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
